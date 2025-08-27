afd2b4752cf2e63e5e76413899ed9259197b4ae5
Zahin1033 <115257876+Zahin1033@users.noreply.github.com>
2025-08-27 11:29:20 +0600

    script added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('script connected')
+// login button fuctionality
+
+document.getElementById("loginButton").addEventListener("click",function(e){
+    e.preventDefault()
+    console.log("login button click")
+    console.log(e)
+})
