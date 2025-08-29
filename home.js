2cfc79ccf79a5ee231ea516544c431bd6cd4f753
Zahin1033 <115257876+Zahin1033@users.noreply.github.com>
2025-08-29 16:36:49 +0600

    cash out added

---
+++ home.js
@@ -0,0 +1,49 @@
+const validPin = 1234
+
+// add money btn function
+document.getElementById("add-money-btn").addEventListener("click", function (e) {
+    e.preventDefault()
+    
+    const bank = document.getElementById("bank").value
+    const accountNumber = document.getElementById("account-number").value
+    const amount = parseInt(document.getElementById("add-amount").value)
+    const pin = parseInt(document.getElementById("add-pin").value)
+
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+
+    if (accountNumber.length < 11) {
+        alert("please provide 11 valid account number")
+        return;
+    }
+    if (pin !== validPin) {
+        alert("Please provide valid pin number")
+        return;
+    }
+    const totalAvailableBalance = amount + availableBalance
+
+    document.getElementById("available-balance").innerText = totalAvailableBalance
+})
+
+
+
+// cashout feature 
+
+document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+    e.preventDefault()
+    
+    const amount = document.getElementById("")
+})
+
+
+// toggling feature 
+
+document.getElementById("add-money").addEventListener("click", function () {
+    document.getElementById("cash-out-parent").style.display = "none"
+    document.getElementById("add-money-parent").style.display = "block"
+})
+document.getElementById("cash-out-button").addEventListener("click", function () {
+
+    document.getElementById("add-money-parent").style.display = "none"
+    document.getElementById("cash-out-parent").style.display = "block"
+})
+
